31e8a198513fdcf1c69693c55c42b560948f9cef
Bogdan Mazoure <bogdan.mazoure@mail.mcgill.ca>
2019-08-10 21:53:03 -0400

    Added PB load

--- model.py
+++ model.py
@@ -37,3 +37,19 @@ pickle.dump(regressor, open('model.pkl','wb'))
 # Loading model to compare the results
 model = pickle.load(open('model.pkl','rb'))
 print(model.predict([[2, 9, 6]]))
+
+# import tensorflow as tf
+
+# def load_graph(frozen_graph_filename):
+#     # We load the protobuf file from the disk and parse it to retrieve the 
+#     # unserialized graph_def
+#     with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+#         graph_def = tf.GraphDef()
+#         graph_def.ParseFromString(f.read())
+
+#     # Then, we import the graph_def into a new Graph and returns it 
+#     with tf.Graph().as_default() as graph:
+#         # The name var will prefix every op/nodes in your graph
+#         # Since we load everything in a new graph, this is not needed
+#         tf.import_graph_def(graph_def, name="prefix")
+#     return graph
